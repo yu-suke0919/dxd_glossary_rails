75c74c18d2b229afcf5b450c40f235ad05e17bbd
yu-suke0919 <yusukekk919@gmail.com>
2026-04-09 15:01:11 +0900

    コントローラーの共通している処理を共通化

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
   def index
     @articles = Article.all
   end
@@ -20,22 +21,24 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    article = Article.find(params[:id])
-    article.update!(article_params)
-    redirect_to articles_url, notice: "#{article.title}を更新しました"
+    @article.update!(article_params)
+    redirect_to articles_url, notice: "#{@article.title}を更新しました"
   end
 
   def destroy
-    article = Article.find(params[:id])
-    article.destroy
-    redirect_to articles_url, notice: "#{article.title}を削除しました"
+    @article.destroy
+    redirect_to articles_url, notice: "#{@article.title}を削除しました"
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :category, :description)
   end
