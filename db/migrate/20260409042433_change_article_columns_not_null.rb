e40583ff06bc071f94a6dda3f3f66cbb214bea08
yu-suke0919 <yusukekk919@gmail.com>
2026-04-09 13:27:18 +0900

    マイグレーションファイルの修正

--- db/migrate/20260409042433_change_article_columns_not_null.rb
+++ db/migrate/20260409042433_change_article_columns_not_null.rb
@@ -1,7 +1,7 @@
 class ChangeArticleColumnsNotNull < ActiveRecord::Migration[8.1]
   def change
-    change_column_null :article, :title, false
-    change_column_null :article, :category, false
-    change_column_null :article, :description, false
+    change_column_null :articles, :title, false
+    change_column_null :articles, :category, false
+    change_column_null :articles, :description, false
   end
 end
