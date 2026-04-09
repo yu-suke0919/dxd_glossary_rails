5327db3bd5a8d245e8d2274804a1c18213170a2d
yu-suke0919 <yusukekk919@gmail.com>
2026-04-09 13:28:07 +0900

    ChangeArticleColumnsNotNullマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_08_041532) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_09_042433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "category"
+    t.string "category", null: false
     t.datetime "created_at", null: false
-    t.text "description"
-    t.string "title"
+    t.text "description", null: false
+    t.string "title", null: false
     t.datetime "updated_at", null: false
   end
 end
