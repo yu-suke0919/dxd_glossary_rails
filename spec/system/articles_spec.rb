dd831d2a3a2199e4f795e4f0be027c649f783e91
yu-suke0919 <yusukekk919@gmail.com>
2026-04-10 10:47:23 +0900

    【テスト未通過】githubにおいてテストが実行されるかテスト

---
+++ spec/system/articles_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe '記事表示機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # 記事Aの作成
+      FactoryBot.create(:article)
+    end
+    context '表示できるか' do
+      before do
+        # ログインしない
+        visit articles_path
+      end
+      it '記事が表示される' do
+        # 記事が画面上に表示されていることを確認
+        expect(page).to have_content 'taitore'
+      end
+    end
+  end
+end
