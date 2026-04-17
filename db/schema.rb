ec3f0378dc3a348a90d4adace2d0e941bddae54f
yu-suke0919 <yusukekk919@gmail.com>
2026-04-17 14:18:20 +0900

    マイグレートの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_10_052513) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_17_025642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_10_052513) do
     t.text "description", null: false
     t.string "title", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["title"], name: "index_articles_on_title", unique: true
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
