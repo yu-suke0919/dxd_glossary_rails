88b6d0c0070be42046ac6aabdfc7a273366e91c4
yu-suke0919 <yusukekk919@gmail.com>
2026-04-17 14:15:12 +0900

    マイグレーションファイルの　up/down処理を追記

--- db/migrate/20260417025642_add_user_id_to_articles.rb
+++ db/migrate/20260417025642_add_user_id_to_articles.rb
@@ -1,4 +1,10 @@
 class AddUserIdToArticles < ActiveRecord::Migration[8.1]
-  def change
+  def up
+    execute 'DELETE FROM articles;'
+    add_reference :articles, :user, null: false, index: true
+  end
+
+  def down
+    remove_reference :articles, :user, index: true
   end
 end
