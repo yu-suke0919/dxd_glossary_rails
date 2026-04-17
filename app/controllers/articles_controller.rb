d9e05c110898e82e194d7e3203364be660cd7321
yu-suke0919 <yusukekk919@gmail.com>
2026-04-17 14:58:27 +0900

    Article作成時にログイン中のUserに関連つけるよう修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.new(article_params)
     if @article.save
       redirect_to articles_url, notice: "「#{@article.title}」を登録しました"
     else
