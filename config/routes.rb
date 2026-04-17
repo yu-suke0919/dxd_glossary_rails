548939a7412cb27f114f606f689269a31a2f8be9
yu-suke0919 <yusukekk919@gmail.com>
2026-04-17 11:26:21 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   root to: "articles#index"
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
