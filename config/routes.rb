bbc855717ae73d0c2cd662559281a6ea37c8b31b
yu-suke0919 <yusukekk919@gmail.com>
2026-04-17 11:08:48 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   root to: "articles#index"
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
