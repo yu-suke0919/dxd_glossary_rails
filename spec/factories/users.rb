cd4404c1ca63979b5c15dc27d56b02f1500eca73
yu-suke0919 <yusukekk919@gmail.com>
2026-04-20 11:28:06 +0900

    factoryBotのUser生成時にパスワードダイジェストを付与するのではなく、パスワードと再入力パスワードを持つように変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
+    name { "test_name" }
+    email { "test_email" }
+    password { "test_password" }
+    password_confirmation { "test_password" }
   end
 end
