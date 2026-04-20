f38e60656730b4b10112f187bd62c3692db2de9d
yu-suke0919 <yusukekk919@gmail.com>
2026-04-20 14:24:48 +0900

    commentsにarticle参照を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_20_043235) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_20_050641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,10 +26,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_20_043235) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.bigint "article_id", null: false
     t.text "body", null: false
     t.integer "comment_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_20_043235) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "comments", "articles"
 end
