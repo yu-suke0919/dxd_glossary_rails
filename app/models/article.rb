5cf279d2d4458fe302b685ebc879004abee208f2
yu-suke0919 <yusukekk919@gmail.com>
2026-04-21 14:41:58 +0900

    ransackにて検索に必要なモデルの属性を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,12 @@ class Article < ApplicationRecord
 
   belongs_to :user
   has_many :comments
+
+  scope :recent, -> { order(created_at: :desc) }
+  def self.ransackable_attributes(auth_object = nil)
+    %w[title body created_at]
+  end
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
