b07c0c94e97f7a8c719d947d11e7fd2bc34ddae5
yu-suke0919 <yusukekk919@gmail.com>
2026-04-21 11:31:36 +0900

    記事詳細ページにてコメント投稿をした時のcreateルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   root to: "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments, only: [ :create ]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
