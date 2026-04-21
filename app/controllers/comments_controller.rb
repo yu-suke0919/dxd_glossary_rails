9cfeb5536320e51515ee62b2d277de7237312295
yu-suke0919 <yusukekk919@gmail.com>
2026-04-21 11:32:02 +0900

    commentコントローラ#createを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
   def create
-      @comment
+      @article = Article.find(params[:article_id])
+      @comment = Comment.new(comment_params)
+      @comment.save!
+      redirect_to articles_url(@article), notice: "コメントを投稿しました"
   end
   private
   def comment_params
-    params.require(:comment).permit(:title)
+    params.require(:comment).permit(:body)
   end
 end
