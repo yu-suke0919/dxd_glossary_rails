74c0d1d5a9d25fd0bbbce34f3a4bccb4d628241c
yu-suke0919 <yusukekk919@gmail.com>
2026-04-21 12:45:34 +0900

    コントローラーにコメント記述のための変数を追記

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   def create
       @article = Article.find(params[:article_id])
-      @comment = Comment.new(comment_params)
-      @comment.save!
-      redirect_to articles_url(@article), notice: "コメントを投稿しました"
+      @comment = @article.comments.new(comment_params)
+      @comment.comment_number = 4
+      if @comment.save
+        redirect_to article_url(@article), notice: "コメントを投稿しました"
+      end
   end
   private
   def comment_params
