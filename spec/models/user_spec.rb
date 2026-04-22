a555901113b65c01190c1e4608115db0058631ab
yu-suke0919 <yusukekk919@gmail.com>
2026-04-22 10:47:28 +0900

    add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー名' do
+    it '1文字以上の場合、Userは有効であること' do
+      user = User.new(name: 'aaa', email: 'email', password: 'pass', password_confirmation: 'pass')
+      expect(user).to be_valid
+    end
+  end
+  
+  describe 'ユーザー名' do
+    it '未入力の場合、Userは無効であること' do
+      user = User.new(name: '', email: 'email', password: 'pass', password_confirmation: 'pass')
+      expect(user).to_not be_valid
+    end
+  end
+  
 end
