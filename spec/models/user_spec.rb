da8df5264e1bf4d6b6d60dcceb0427a331fbb337
yu-suke0919 <yusukekk919@gmail.com>
2026-04-22 11:28:06 +0900

    余計な空白を削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe User, type: :model do
       user = User.new(name: 'aaa', email: 'email', password: '', password_confirmation: '')
       expect(user).to be_invalid
     end
-     it '7文字以下の場合、Userは無効であること' do
+    it '7文字以下の場合、Userは無効であること' do
       user = User.new(name: 'aaa', email: 'email', password: 'a' * 7, password_confirmation: 'a' * 7)
       expect(user).to be_invalid
     end
