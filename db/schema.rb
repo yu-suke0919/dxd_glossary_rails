f0e99fb8e9afbc0d7467554893383c2cdc7c560c
yu-suke0919 <yusukekk919@gmail.com>
2026-04-23 10:17:46 +0900

    comments に user 外部キーを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_21_041720) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_23_010754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,7 +32,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_21_041720) do
     t.integer "comment_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_21_041720) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
