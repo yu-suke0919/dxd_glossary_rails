59c9f3cb79c39ac552f4806da04a564347c47081
yu-suke0919 <yusukekk919@gmail.com>
2026-04-24 14:41:31 +0900

    commentの検証が失敗した時にエラー文を表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :current_user
   def create
     @article = Article.find(params[:article_id])
-    @comment = Comments::CreateService.new.call(article: @article, user: current_user, params: comment_params)
+    @comment = Comments::CreateService.new.call(article: @article, user: @current_user, params: comment_params)
     if @comment.persisted?
       redirect_to article_url(@article), notice: "コメントを投稿しました"
     else
